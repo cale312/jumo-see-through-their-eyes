5cebbaa9ca1268fef710134768d0ce9de0926a4e
Callum Fraser <callum@projectcodex.co>
2017-06-13 16:49:43 +0200

    added sophia file

---
+++ personas/sophia.js
@@ -0,0 +1,41 @@
+var sophia = {
+  budgetAmount : 1500,
+  instructions : "Try to make it through to the end of the month without exceeding your budget.",
+  mandatoryExpenses: {
+    "Rent" : 240,
+    "School Fees" : 160,
+    "Transport" : 400,
+    "Water and electricity" : 120
+  },
+  weekOneOptions : {
+    "Bread" : 15,
+    "Milk" : 20,
+    "Rice" : 60,
+    "Sugar" : 45
+  },
+  weekTwoOptions : {
+    "Chicken" : 120,
+    "Bread" : 20,
+    "Butternut" : 15,
+    ""
+  },
+  weekThreeOptions : {
+    "Bread" : 15,
+    "Milk" : 20,
+    "Rice" : 60,
+    "Sugar" : 45
+  },
+  weekFourOptions : {
+    "Bread" : 15,
+    "Milk" : 20,
+    "Rice" : 60,
+    "Sugar" : 45
+  },
+  story : "Sophia is a single mother of three from Kinshasa in the Democratic Republic of Congo." +
+          "She has a very tight budget which does not allow for " +
+          "unplanned spending. A surprise expense, such as a medical bill, could mean the end of her" +
+          "allowance for food and water for the month. She uses JUMO for situations where she needs money fast, but" +
+          "she often has to pay the late penalty for significant unanticipated fees. She would be interested in a" +
+          "medical aid product or medical loan product in the future.",
+  image : (link)
+}
