a59730651f23e9c57ece83b232c4dc771a51e2cd
Callum Fraser <callum@projectcodex.co>
2017-06-14 16:31:58 +0200

    added background pictures

--- personas/sophia.js
+++ personas/sophia.js
@@ -1,128 +1,125 @@
 var sophia = {
-  name : "Sophia",
-  budgetAmount : 1500,
-  instructions : "Try to make it through to the end of the month without exceeding your budget.",
-  mandatoryExpenses: {
-    "Rent" : 60,
-    "School Fees" : 40,
-    "Transport" : 100,
-    "Water and electricity" : 30
-  },
-  unexpectedexpense: {
-    "text" : "Sophias son broke his leg while playing soccer for his high school team., and the doctors fee totals at 400",
-    "expense" : 400
-  },
-  weekOneOptions : [
-    {
-        productName : "Bread",
-        price       : 15,
-        image       : "link"
-
+    name: "Sophia",
+    budgetAmount: 1500,
+    instructions: "You are a single mother of three. " +
+        "Try to make it through to the end of the month without exceeding your budget.",
+    mandatoryExpenses: {
+        "Rent": 60,
+        "School Fees": 40,
+        "Transport": 100,
+        "Water and electricity": 30
     },
-    {
-        productName : "Milk",
-        price       : 20,
-        image       : "link"
-
+    unexpectedexpense: {
+        "text": "Sophias son broke his leg while playing soccer for his high school team., and the doctors fee totals at 400",
+        "expense": 400
     },
-    {
-        productName : "Rice",
-        price       : 60,
-        image       : "link"
+    weekOneOptions: [{
+            productName: "Bread",
+            price: 15,
+            image: "link"
 
-    },
-    {
-        productName : "Sugar",
-        price       : 45,
-        image       : "link"
+        },
+        {
+            productName: "Milk",
+            price: 20,
+            image: "link"
 
-    }
-  ],
-  weekTwoOptions : [
-    {
-        productName : "Chicken",
-        price       : 120,
-        image       : "link"
+        },
+        {
+            productName: "Rice",
+            price: 60,
+            image: "link"
 
-    },
-    {
-        productName : "Bread",
-        price       : 15,
-        image       : "link"
+        },
+        {
+            productName: "Sugar",
+            price: 45,
+            image: "link"
 
-    },
-    {
-        productName : "Butternut",
-        price       : 30,
-        image       : "link"
+        }
+    ],
+    weekTwoOptions: [{
+            productName: "Chicken",
+            price: 120,
+            image: "link"
 
-    },
-    {
-        productName : "Watermelon",
-        price       : 35,
-        image       : "link"
+        },
+        {
+            productName: "Bread",
+            price: 15,
+            image: "link"
 
-    },
-  ],
-  weekThreeOptions : [
-    {
-        productName : "Bread",
-        price       : 15,
-        image       : "link"
+        },
+        {
+            productName: "Butternut",
+            price: 30,
+            image: "link"
 
-    },
-    {
-        productName : "Milk",
-        price       : 20,
-        image       : "link"
+        },
+        {
+            productName: "Watermelon",
+            price: 35,
+            image: "link"
 
-    },
-    {
-        productName : "rice",
-        price       : 60,
-        image       : "link"
+        },
+    ],
+    weekThreeOptions: [{
+            productName: "Bread",
+            price: 15,
+            image: "link"
 
-    },
-    {
-        productName : "Sugar",
-        price       : 45,
-        image       : "link"
+        },
+        {
+            productName: "Milk",
+            price: 20,
+            image: "link"
 
-    },
-  ],
-  weekFourOptions : [
-    {
-        productName : "Bread",
-        price       : 15,
-        image       : "link"
+        },
+        {
+            productName: "rice",
+            price: 60,
+            image: "link"
 
-    },
-    {
-        productName : "Milk",
-        price       : 20,
-        image       : "link"
+        },
+        {
+            productName: "Sugar",
+            price: 45,
+            image: "link"
 
-    },
-    {
-        productName : "Rice",
-        price       : 60,
-        image       : "link"
+        },
+    ],
+    weekFourOptions: [{
+            productName: "Bread",
+            price: 15,
+            image: "link"
 
-    },
-    {
-        productName : "Sugar",
-        price       : 45,
-        image       : "link"
+        },
+        {
+            productName: "Milk",
+            price: 20,
+            image: "link"
 
-    },
-  ],
-  story : "Sophia is a single mother of three from Kinshasa in the Democratic Republic of Congo." +
-          "She has a very tight budget which does not allow for " +
-          "unplanned spending. A surprise expense, such as a medical bill, could mean the end of her" +
-          "allowance for food and water for the month. She uses JUMO for situations where she needs money fast, but" +
-          "she often has to pay the late penalty for significant unanticipated fees. She would be interested in a" +
-          "medical aid product or medical loan product in the future.",
-  image : "image"
+        },
+        {
+            productName: "Rice",
+            price: 60,
+            image: "link"
+
+        },
+        {
+            productName: "Sugar",
+            price: 45,
+            image: "link"
+
+        },
+    ],
+    story: "Sophia is a single mother of three from Kinshasa in the Democratic Republic of Congo." +
+        "She has a very tight budget which does not allow for " +
+        "unplanned spending. A surprise expense, such as a medical bill, could mean the end of her" +
+        "allowance for food and water for the month. She uses JUMO for situations where she needs money fast, but" +
+        "she often has to pay the late penalty for significant unanticipated fees. She would be interested in a" +
+        "medical aid product or medical loan product in the future.",
+    image: "image"
 }
 
 module.exports = sophia
