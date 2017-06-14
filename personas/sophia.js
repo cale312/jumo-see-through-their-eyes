8f1103aad48271131e29ff45b02ded2985319838
Shannon Nichols <shannonsn19@gmail.com>
2017-06-14 18:47:46 +0200

    fixed typos

--- personas/sophia.js
+++ personas/sophia.js
@@ -1,7 +1,7 @@
 var sophia = {
   name : "Sophia",
   budgetAmount : 1500,
-  instructions : "Try to make it through to the end of the month without exceeding your budget.",
+  instructions : "You are a single mother of three. Try to make it through to the end of the month without exceeding your budget.",
   mandatoryExpenses: {
     "Rent" : 60,
     "School Fees" : 40,
@@ -9,7 +9,7 @@ var sophia = {
     "Water and electricity" : 30
   },
   unexpectedexpense: {
-    "text" : "Sophias son broke his leg while playing soccer for his high school team., and the doctors fee totals at 400",
+    "text" : "Oh no! Sophia's son broke his leg while playing soccer for his high school team, and the doctors fee totals at 400",
     "expense" : 400
   },
   weekOneOptions : [
@@ -115,11 +115,11 @@ var sophia = {
         image       : "../public/img/sugar.jpg"
 
     }],
-    story: "Sophia is a single mother of three from Kinshasa in the Democratic Republic of Congo." +
+    story: "Sophia is a single mother of three from Kinshasa in the Democratic Republic of Congo. " +
         "She has a very tight budget which does not allow for " +
-        "unplanned spending. A surprise expense, such as a medical bill, could mean the end of her" +
-        "allowance for food and water for the month. She uses JUMO for situations where she needs money fast, but" +
-        "she often has to pay the late penalty for significant unanticipated fees. She would be interested in a" +
+        "unplanned spending. A surprise expense, such as a medical bill, could mean the end of her " +
+        "allowance for food and water for the month. She uses JUMO for situations where she needs money fast, but " +
+        "she often has to pay the late penalty for significant unanticipated fees. She would be interested in a " +
         "medical aid product or medical loan product in the future.",
     image: "image"
 }
