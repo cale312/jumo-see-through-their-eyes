0e5a1a034f8250f72b991e8c60f1e997d5127c6c
Callum Fraser <callum@projectcodex.co>
2017-06-14 16:38:00 +0200

    fixed some conflicts

--- personas/sophia.js
+++ personas/sophia.js
@@ -1,19 +1,4 @@
 var sophia = {
-
-    name: "Sophia",
-    budgetAmount: 1500,
-    instructions: "You are a single mother of three. " +
-        "Try to make it through to the end of the month without exceeding your budget.",
-    mandatoryExpenses: {
-        "Rent": 60,
-        "School Fees": 40,
-        "Transport": 100,
-        "Water and electricity": 30
-    },
-    unexpectedexpense: {
-        "text": "Sophias son broke his leg while playing soccer for his high school team., and the doctors fee totals at 400",
-        "expense": 400
-}
   name : "Sophia",
   budgetAmount : 1500,
   instructions : "Try to make it through to the end of the month without exceeding your budget.",
@@ -129,107 +114,7 @@ var sophia = {
         price       : 45,
         image       : "../public/img/sugar.jpg"
 
-    },
-    weekOneOptions: [{
-            productName: "Bread",
-            price: 15,
-            image: "link"
-
-        },
-        {
-            productName: "Milk",
-            price: 20,
-            image: "link"
-
-        },
-        {
-            productName: "Rice",
-            price: 60,
-            image: "link"
-
-        },
-        {
-            productName: "Sugar",
-            price: 45,
-            image: "link"
-
-        }
-    ],
-    weekTwoOptions: [{
-            productName: "Chicken",
-            price: 120,
-            image: "link"
-
-        },
-        {
-            productName: "Bread",
-            price: 15,
-            image: "link"
-
-        },
-        {
-            productName: "Butternut",
-            price: 30,
-            image: "link"
-
-        },
-        {
-            productName: "Watermelon",
-            price: 35,
-            image: "link"
-
-        },
-    ],
-    weekThreeOptions: [{
-            productName: "Bread",
-            price: 15,
-            image: "link"
-
-        },
-        {
-            productName: "Milk",
-            price: 20,
-            image: "link"
-
-        },
-        {
-            productName: "rice",
-            price: 60,
-            image: "link"
-
-        },
-        {
-            productName: "Sugar",
-            price: 45,
-            image: "link"
-
-        },
-    ],
-    weekFourOptions: [{
-            productName: "Bread",
-            price: 15,
-            image: "link"
-
-        },
-        {
-            productName: "Milk",
-            price: 20,
-            image: "link"
-
-        },
-        {
-            productName: "Rice",
-            price: 60,
-            image: "link"
-
-        },
-        {
-            productName: "Sugar",
-            price: 45,
-            image: "link"
-
-        },
-    ],
+    }],
     story: "Sophia is a single mother of three from Kinshasa in the Democratic Republic of Congo." +
         "She has a very tight budget which does not allow for " +
         "unplanned spending. A surprise expense, such as a medical bill, could mean the end of her" +
